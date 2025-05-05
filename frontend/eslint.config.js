dc85bfdf0de06c4ad70e5abd8f8f5f24b36e47d2
patel-miten <mpatel@supplyhive.com>
2025-05-05 23:53:55 +0530

    feat : addd prettier , tailwind and eslint ruls

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,18 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: [
+      'dist',
+      "build/*",
+      ".yarn/*",
+      ".github/*",
+      "**/node_modules",
+      "**/package.json",
+      "**/vite.config.ts",
+      "libs/*",
+      "libs-jdp/*",]
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -23,6 +34,35 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "import/imports-first": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["error", {
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      }],
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "react/display-name": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/ban-types": "off",
+      "react/jsx-key": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "react/prop-types": "off",
+      "prefer-const": "off",
+      "react/no-children-prop": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "no-var": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/ban-ts-ignore": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/camelcase": "off",
+      "@typescript-eslint/class-name-casing": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
     },
   },
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const envPrefix = 'REACT_APP_';
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), envPrefix) };
+  const host = process.env.REACT_APP_HOST || 'localhost';
+  const port = Number(process.env.REACT_APP_PORT || 3000);
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),svgr()],
-  
+  return {
+    plugins: [react(), svgr(),tailwindcss()],
+    server: {
+      host,
+      port,
+      strictPort: true, //  exit if port is already in use
+      open: true, // automatically open the app in the browser on server start
+    },
+  }
 })
+
