329db4e9f6d8dc348f1d75f8ad08a535dc22fb70
patel-miten <mpatel@supplyhive.com>
2025-06-05 01:14:18 +0530

    feat : compnay details add
    wip * clean code

--- frontend/src/pages/my-space/profile-info/index.tsx
+++ frontend/src/pages/my-space/profile-info/index.tsx
@@ -1,10 +1,8 @@
-import { useTranslation } from "react-i18next";
 import UserInfoCard from "./UserInfoCard";
-import UserAddressCard from "./UserAddressCard";
+import UserCompanyCard from "./UserCompanyCard";
 import UserMetaCard from "./UserMetaCard";
 
 const ProfileInfo = () => {  
-  const {t}= useTranslation()
   return (
     <>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
@@ -14,7 +12,7 @@ const ProfileInfo = () => {
         <div className="space-y-6">
           <UserMetaCard/>
           <UserInfoCard />
-          <UserAddressCard /> 
+          <UserCompanyCard /> 
         </div>
       </div>
     </>
